7711ccb86cf17e00dc97452f0fbacec18dfc6ea9
Richard Kurwitz <kurwitz3@aol.com>
2020-08-20 02:15:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,11 @@
 def prime?(int)
+  (2..(int - 1)).each do |x|
+    return false if int % x == 0 
 
+end
+def is_prime(num)
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  true
 end
